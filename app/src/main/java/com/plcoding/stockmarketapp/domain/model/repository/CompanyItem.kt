95b0e8d80d0b3c44b3a88c5fdebce6f048913a25
saul.wiggin <saulwiggin@googlemail.com>
2022-04-26 14:56:27 +0100

    Composable Company Item ListView and Listings Screen

--- app/src/main/java/com/plcoding/stockmarketapp/domain/model/repository/CompanyItem.kt
+++ app/src/main/java/com/plcoding/stockmarketapp/domain/model/repository/CompanyItem.kt
@@ -1,18 +1,18 @@
 package com.plcoding.stockmarketapp.domain.model.repository
 
 import android.text.Layout
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterVertically
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import java.lang.reflect.Modifier
 
 @Composable
 fun CompanyItem (
@@ -21,28 +21,40 @@ fun CompanyItem (
 ){
     Row(
         modifier = modifier,
-        verticalAlignment = Layout.Alignment.CenterVertically
+        verticalAlignment = Alignment.CenterVertically
     ){
-        Text(
-            text = company.name,
-            fontweight = FontWeight.Bold,
-            fontSize = 16.sp,
-            color = MaterialTheme.colors.onBackground,
-            overflow = TextOverflow.Ellipsis,
-            maxLines = 1,
-            modifier = Modifier.weight(1f)
-        )
-        Spacer(modifier = Modifer.width(4.dp))
-        Text(
-            text = company.exchange,
-            fontWeight = fontWeight.Light,
-            color = MaterialTheme.colors.onBackground
-        )
-    }
-    Spacer(modifier = Modifier.height(8.dp))
-    Text(
-        text = "(${company.symbol})",
-        fontStyle = FontStyle.Italic,
-        color =
-    )
+        Column(
+            modifier = modifier.weight(1f)
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = company.name,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 16.sp,
+                    color = MaterialTheme.colors.onBackground,
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 1,
+                    modifier = Modifier.weight(1f)
+                )
+                Spacer(modifier = Modifier.width(4.dp))
+                Text(
+                    text = company.exchange,
+                    fontWeight = FontWeight.Light,
+                    color = MaterialTheme.colors.onBackground
+                )
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = "(${company.symbol})",
+                fontStyle = FontStyle.Italic,
+                //color = MaterialTheme.
+            )
+        }
 }
+
+
+}
+
+
--- app/src/main/java/com/plcoding/stockmarketapp/domain/model/repository/CompanyListingsScreen.kt
+++ app/src/main/java/com/plcoding/stockmarketapp/domain/model/repository/CompanyListingsScreen.kt
@@ -1,6 +1,12 @@
 package com.plcoding.stockmarketapp.domain.model.repository
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Divider
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.dp
@@ -8,6 +14,7 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.room.util.TableInfo
 import com.google.accompanist.swiperefresh.SwipeRefresh
 import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
+import com.plcoding.stockmarketapp.presentation.company_info.company_listings.CompanyListingEvent
 import com.plcoding.stockmarketapp.presentation.company_info.company_listings.CompanyListingsViewModel
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
@@ -17,6 +24,7 @@ import java.lang.reflect.Modifier
 @Composable
 @Destination(start = true)
 fun CompanyListingsScreen(
+    modifier: androidx.compose.ui.Modifier = androidx.compose.ui.Modifier,
     navigator: DestinationsNavigator,
     viewModel: CompanyListingsViewModel = hiltViewModel()
 ) {
@@ -24,45 +32,49 @@ fun CompanyListingsScreen(
         isRefreshing = viewModel.state.isRefreshing
     )
     val state = viewModel.state
-    TableInfo.Column(
-        modifier = Modifier.fillMaxSize()
+    Column(
+        modifier = modifier.fillMaxSize()
     ) {
         OutlinedTextField(
             value = state.searchQuery,
             onValueChange = {
                 viewModel.onEvent(
-                    CompanyListingsEvent.OnSearchQueryChange(it)
+                    CompanyListingEvent.OnSearchQueryChange(it)
                 )
             },
-            modifier = Modifier
+            modifier = modifier
                 .padding(16.dp)
-                .fillmaxwidth(),
+                .fillMaxWidth(),
                 placeholder = {
-                    Text(text = "search...")
+                    Text(text = "Search...")
                 },
                 maxlines = 1,
                 singleLine = true
-        ))
+        )
             SwipeRefresh(
                 state = swipeRefreshState,
                 onRefresh = {
-                    viewModel.onEvent(CompanyListingsEvent.Refresh)
+                    viewModel.onEvent(CompanyListingEvent.Refresh)
                 }
         ) {
                 LazyColumn(
-                    modifier = Modifier.fillMaxWidth()
+                    modifier = modifier.fillMaxWidth()
                 ) {
-                    items(state.companies.size){
-                        if(i < state.companies.size) { i ->
-                            CompanyItem(
-                                company = company,
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .clickable {
-                                        // TODO: Navigate to
-                                    }
-                                    .padding
-                            )
+                    items(state.companies.size){ i ->
+                        val company = state.companies[i]
+                        CompanyItem(
+                            company = company,
+                            modifier = modifier
+                                .fillMaxWidth()
+                                .clickable {
+                                    // TODO: Navigate to detail screen
+                                }
+                                .padding(16.dp)
+                        )
+                        if(i < state.companies.size) {
+                            Divider(modifier = modifier.padding(
+                               horizontal = 16.dp
+                           ))
                     }
                 }
             }
